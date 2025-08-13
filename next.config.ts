a538a9ce0696724462fee173e4a4aabbb89c4f4a
filipego <filipego@gmail.com>
2025-08-13 18:30:33 -0400

    added cloudflare

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.midjourney.com", "imagedelivery.net"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     // Disable type checking during build (use local dev/typecheck CI instead)
     ignoreBuildErrors: true,
